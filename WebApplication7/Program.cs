8f1bd93fe66852a8347c48211bab5e2c716d8f2d
VanessaLin9 <vanessa7591@gmail.com>
2025-01-15 03:01:55 +0800

    feat: add log

--- WebApplication7/Program.cs
+++ WebApplication7/Program.cs
@@ -1,9 +1,20 @@
 using WebApplication7.Endpoints;
+using XitMent.Core.Serilog;
+using XitMent.Core.Serilog.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
+
+builder.UseXitMentSerilog(new SerilogSetting(
+    Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT").ToDeployEnvironment(),
+    builder.Configuration["Project"]!,
+    Environment.GetEnvironmentVariable("POD_NAME"),
+    Environment.GetEnvironmentVariable("NODE_NAME"),
+    Environment.GetEnvironmentVariable("ELK_USERNAME"),
+    Environment.GetEnvironmentVariable("ELK_PASSWARD")
+));
 
 var app = builder.Build();
 
